a0ced577ead5d2a7c807131126206f1eb85a64ed
Rajath Acharya <rajath365@gmail.com>
2021-08-25 00:31:34 +0530

    feat: header init

--- pages/dashboard/index.jsx
+++ pages/dashboard/index.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import Navbar from '../../components/Navbar';
+import Header from '../../shared/Header';
 
 const Dashboard = () => (
-  <Navbar />
+  <Header />
 );
 
 export default Dashboard;
---
+++ shared/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import styles from '../styles/shared/header.module.scss';
+
+const Header = () => (
+  <header className={styles.header}>
+    <seaction>
+      logo
+    </seaction>
+    <seaction>
+      nav
+    </seaction>
+    <seaction>
+      search
+    </seaction>
+  </header>
+);
+
+export default Header;
